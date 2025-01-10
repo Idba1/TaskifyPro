1acde3ed2d389d140963de2ff66d90ec7614e88f
Idba1 <moniraislamself@gmail.com>
2025-01-11 03:04:22 +0600

    Handling proper task list and task deletion

--- src/components/tasks/TaskCard.jsx
+++ src/components/tasks/TaskCard.jsx
@@ -1,6 +1,6 @@
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { ArrowRightIcon, TrashIcon } from '@heroicons/react/24/outline';
 import { useDispatch } from 'react-redux';
-import { updateStatus } from '../../redux/features/tasks/tasksSlice';
+import { removeTask, updateStatus } from '../../redux/features/tasks/tasksSlice';
 
 const TaskCard = ({ task }) => {
   const dispatch = useDispatch();
@@ -31,6 +31,9 @@ const TaskCard = ({ task }) => {
       <div className="flex justify-between mt-3">
         <p>{task?.date}</p>
         <div className="flex gap-3">
+          <button onClick={() => dispatch(removeTask(task.id))} title="Delete">
+            <TrashIcon className="h-5 w-5 text-red-500" />
+          </button>
           <button
             onClick={() =>
               dispatch(updateStatus({ id: task.id, status: updatedStatus }))
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "./features/user/userSlice";
 import reducer from "./features/tasks/tasksSlice";
 
 const store = configureStore({
     reducer: {
         tasksSlice: reducer,
+        userSlice: userSlice,
     },
 })
 
